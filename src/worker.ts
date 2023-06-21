d36c053a2764143400d9d85f31b892e1afb616ed
Nicolo Traini <nicolo.traini@oncode.it>
2023-06-21 11:33:09 +0200

    rearrange functions and add simple echo command

--- src/worker.ts
+++ src/worker.ts
@@ -13,44 +13,49 @@ import { Env } from "../models/env"
 import { Message } from "../models/message"
 import { User } from "../models/user"
 
-async function listRymUsers(message: Message, env: Env) {
-  const chatId = message.chat.id
-  const rymUserIds = await env.RYM_USERS
-    .list()
-    .then(list => list.keys.map(key => key.name))
-  const chatMembers: User[] = await Promise.all(rymUserIds
-    .map(userId => `https://api.telegram.org/bot${env.API_KEY}/getChatMember?chat_id=${chatId}&user_id=${userId}`)
-    .map(url => fetch(url)
-      .then(resp => resp.json())
-      .then(resp => (resp as ChatMemberResponse).result.user)))
-  const rymUsers = await Promise.all(rymUserIds.map(userId => env.RYM_USERS
-    .get(`${userId}`)
-    .then(profileUrl => ({ username: chatMembers.find(cm => `${cm.id}` === userId)?.username ?? 'unknown username', profileUrl }))))
-  const renderedRymUsersList = rymUsers
-    .map(rymUser => `${rymUser.username}\n${rymUser.profileUrl}`)
-    .join('\n\n')
-  const text = `Profili RYM salvati:\n${renderedRymUsersList}`
-  const url = `https://api.telegram.org/bot${env.API_KEY}/sendMessage?chat_id=${chatId}&text=${text}`
-  const data = await fetch(url).then(resp => resp.json())
-}
-
-async function handleRequest(request: Request, env: Env) {
-  if (request.method === "POST") {
-    const payload: any = await request.json()
-    if ('message' in payload) {
-      const message: Message = payload.message
-      if (message.text === 'rym') {
-        await listRymUsers(message, env)
-      }
-    }
-  }
-
-  return new Response(`pippo franco`);
-}
-
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-    const response = handleRequest(request, env)
-    return response
+    async function listRymUsers(message: Message) {
+      const chatId = message.chat.id
+      const rymUserIds = await env.RYM_USERS
+        .list()
+        .then(list => list.keys.map(key => key.name))
+      const chatMembers: User[] = await Promise.all(rymUserIds
+        .map(userId => `https://api.telegram.org/bot${env.API_KEY}/getChatMember?chat_id=${chatId}&user_id=${userId}`)
+        .map(url => fetch(url)
+          .then(resp => resp.json())
+          .then(resp => (resp as ChatMemberResponse).result.user)))
+      const rymUsers = await Promise.all(rymUserIds.map(userId => env.RYM_USERS
+        .get(`${userId}`)
+        .then(profileUrl => ({ username: chatMembers.find(cm => `${cm.id}` === userId)?.username ?? 'unknown username', profileUrl }))))
+      const renderedRymUsersList = rymUsers
+        .map(rymUser => `${rymUser.username}\n${rymUser.profileUrl}`)
+        .join('\n\n')
+      const text = `Lista profili RYM salvati:\n${renderedRymUsersList}`
+      const url = `https://api.telegram.org/bot${env.API_KEY}/sendMessage?chat_id=${chatId}&text=${text}`
+      const data = await fetch(url).then(resp => resp.json())
+    }
+
+    async function echo(message: Message) {
+      const chatId = message.chat.id
+      const content = message.text
+      const text = `${content}!`
+      const url = `https://api.telegram.org/bot${env.API_KEY}/sendMessage?chat_id=${chatId}&text=${text}`
+      const data = await fetch(url).then(resp => resp.json())
+    }
+
+    if (request.method === "POST") {
+      const payload: any = await request.json()
+      if ('message' in payload) {
+        const message: Message = payload.message
+        if (message.text === 'rym') {
+          await listRymUsers(message)
+        } else if (message.text === 'echo') {
+          await echo(message)
+        }
+      }
+    }
+
+    return new Response(`pippo franco`);
 	},
 };
